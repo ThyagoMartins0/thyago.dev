c3bccf5d124e7a5a955eaa746b94be3e078cf60d
Thyago Christian Ramos Martins <thyago.martins@esamcsantos.onmicrosoft.com>
2024-01-14 16:57:06 -0300

    #

--- index.js
+++ index.js
@@ -10,6 +10,22 @@ function mostrarBotoes() {
       botoesContainer.style.display = 'none';
   }
 }
+
+/* ------------------- */
 window.onload = () => {
   topo.classList.toggle ("hide-top");
 }
+
+/*-------------------*/
+function scrollToSection(navbar, about) {
+  // Atraso de 2 segundos antes de iniciar a rolagem
+  setTimeout(function() {
+      // Obtém o elemento alvo com base no ID
+      const targetElement = document.getElementById(about);
+
+      // Usa o método scrollIntoView para rolar até o elemento alvo
+      targetElement.scrollIntoView();
+  }, 2000); // 2000 milissegundos = 2 segundos
+}
+
+
