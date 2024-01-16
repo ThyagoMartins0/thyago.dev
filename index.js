bc43e1923cebf89abe7c73e62210bf3f7eed756a
Thyago Christian Ramos Martins <thyago.martins@esamcsantos.onmicrosoft.com>
2024-01-15 21:30:48 -0300

    #

--- index.js
+++ index.js
@@ -17,15 +17,5 @@ window.onload = () => {
 }
 
 /*-------------------*/
-function scrollToSection(navbar, about) {
-  // Atraso de 2 segundos antes de iniciar a rolagem
-  setTimeout(function() {
-      // Obtém o elemento alvo com base no ID
-      const targetElement = document.getElementById(about);
-
-      // Usa o método scrollIntoView para rolar até o elemento alvo
-      targetElement.scrollIntoView();
-  }, 2000); // 2000 milissegundos = 2 segundos
-}
 
 
