7afbe1cda3e908bc8b02fdb11be1c51b9cb16ce6
Thyago Martins <oficialthyago2@gmail.com>
2024-08-02 01:18:38 -0300

    #

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <SpeedInsights />
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
